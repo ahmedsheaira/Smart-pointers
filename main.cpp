315e8559ee7666190feb75dfefdfa5f37bc4f7fe
ahmedsheaira <sheaira.ahmed@gmail.com>
2024-04-20 21:37:13 +0200

    Test Unique Pointers in C++

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ int main() {
     cout << *myint << endl;
 
     cout << "This is Unique Pointer Demo!!" << endl;
-    // unique_ptr<Foo> f(new Foo(10));
-    // cout << f->get_x() << endl;
-    unique_ptr<Foo> uptr1(new Foo(10));
+    unique_ptr<UniqueFoo> f(new UniqueFoo(10));
+    cout << f->get_x() << endl;
+    unique_ptr<UniqueFoo> uptr1(new UniqueFoo(10));
     /* 
      * Should use make_unique because it is exception safe, if you are creating an object and there is some exception,
      * it will handle that
      */
-    unique_ptr<Foo> uptr2 = make_unique<Foo>(20);  
+    unique_ptr<UniqueFoo> uptr2 = make_unique<UniqueFoo>(20);  
     cout << uptr1->get_x() << (*uptr2).get_x() << endl;
 
     // uptr1 = uptr2; FAIL : This will fail beacuse you can not copy ownership.
-    unique_ptr<Foo> uptr3 = std::move(uptr1);  // It will pass because moving ownership is allowed.
+    unique_ptr<UniqueFoo> uptr3 = std::move(uptr1);  // It will pass because moving ownership is allowed.
     cout << uptr3->get_x() << endl;
 
     return 0;
